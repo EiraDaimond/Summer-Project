e1888f71410d09e7284503baf84362e1cfa6033b
EiraDaimond <s2714361@ed.ac.uk>
2026-06-30 19:05:33 +0000

    struggling

--- RMHMC.py
+++ RMHMC.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import time 
+import time
+from tqdm import tqdm
 
 class RMHMC:
     '''
@@ -33,33 +34,26 @@ class RMHMC:
 
     # Run the HMC algorithm
     def RMHMC_alg(self, n):
-        print(time.time())
         # Initialise the x values
         x = [0]
         # Start the loop to generate x values
         for t in range(n+1):
-            # Initialise the x_star and p_star lists
-            x_star = []
-            p_star = []
-            # Draw the momentum from a Normal distribution and append values to the lists
-            p_star.append(np.random.normal(0, np.abs(self.G(x[t]))))
-            x_star.append(x[t])
-            # Compute the first leapfrog step and append values to the lists
-            p_star.append(1/(-3*(self.eps/self.G(x[t])**2)*self.lam*x[t])*(-1 - (1 + 6*self.lam*x[t]*self.eps/self.G(x[t])**2*(-p_star[0] +0.5*self.eps*self.k*x[t] + 0.5*self.eps*self.lam*x[t]**3 + 0.25*self.eps*(1/self.G(x[t]))*(-6)*self.lam*x[t])**0.5)))
-            x_star.append(x_star[0] + self.eps*self.G(x_star[0])*p_star[0])
+            # Compute the first leapfrog step
+            p_star = p - 0.5*self.eps*(self.k*x[t] + self.lam*x[t]**3 + 0.5*p_star**2(-6*self.lam*x[t]/self.G(x[t])**2)+ 0.5/2*np.pi(-6*lam*x[t]))
+            x_star = [x[t] + self.eps*self.G(x[t])*p_star]
             # Compute (x*, - p*) using L leapfrog steps of size eps
-            for l in range(2, self.L):
-                p_star.append(1/(-6*(self.eps/self.G(x_star[l-1])**2)*self.lam*x_star[l-1])*(-1 - (1 + 2*6*self.lam*x_star[l-1]*self.eps/self.G(x_star[l-1])**2*(-p_star[l-1] + self.eps*self.k*x_star[l-1] + self.eps*self.lam*x_star[l-1]**3 + 0.5*self.eps*(1/self.G(x_star[l-1]))*(-6)*self.lam*x_star[l-1])**0.5))
+            for l in range(1, self.L):
+                p_star_intermediary  = p_star - 0.5*self.eps*(self.k*x[t] + self.lam*x[t]**3 + 0.5*p_star**2(-6*self.lam*x[t]/self.G(x[t])**2)+ 0.5/2*np.pi(-6*lam*x[t]))
                 x_star.append(x_star[l-1] + self.eps*self.G(x_star[l-1])*p_star[l])
             # Compute the final step of the leapfrog method and append to the list
-            p_star.append(1/(-3*(self.eps/self.G(x_star[self.L])**2)*self.lam*x_star[self.L])*(-1 - (1 + 6*self.lam*x_star[self.L]*self.eps/self.G(x_star[self.L])**2*(-p_star[self.L] +0.5*self.eps*self.k*x_star[self.L] + 0.5*self.eps*self.lam*x_star[self.L]**3 + 0.25*self.eps*(1/self.G(x_star[self.L]))*(-6)*self.lam*x_star[self.L])**0.5))
+            p_star.append(1/(-3*(self.eps/self.G(x_star[self.L-1])**2)*self.lam*x_star[self.L-1])*(-1 - (1 + 6*self.lam*x_star[self.L-1]*self.eps/self.G(x_star[self.L-1])**2*(abs(-p_star[self.L-1] +0.5*self.eps*self.k*x_star[self.L-1] + 0.5*self.eps*self.lam*x_star[self.L-1]**3 + 0.25*self.eps*(1/self.G(x_star[self.L-1]))*(-6)*self.lam*x_star[self.L-1]))**0.5)))
             # Compute the acceptance ratio
-            r = np.exp(-self.H(x_star[self.L], p_star[self.L+1]) + self.H(x_star[0], p_star[0]))
+            r = np.exp(-self.H(x_star[self.L-1], p_star[self.L]) + self.H(x_star[0], p_star[0]))
             # Draw W from a Uniform distribution
             W = np.random.uniform(0, 1)
             # Carry out the Metropolis test
             if W <= min(1, r):
-                x.append(x_star[self.L+1])
+                x.append(x_star[self.L-1])
             else:
                 x.append(x[t])
         return x
@@ -72,5 +66,5 @@ def exp_val(x):
     return np.mean(x)
 
 # Testing the code
-RMHMC_test = RMHMC(L=10, eps=0.1, k=1, lam=1)
+RMHMC_test = RMHMC(L=10, eps=0.01, k=1, lam=1)
 print(RMHMC_test.RMHMC_alg(10))
