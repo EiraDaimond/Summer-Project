8fd6fba52eb91de766a1ed7c0a63a294e65aebf8
Eira is my\ best\ girlfriend <grapes@junk.com>
2026-07-08 11:17:10 +0100

    update

--- RMHMC_attempt_2.py
+++ RMHMC_attempt_2.py
@@ -22,7 +22,13 @@ class RMHMC:
 
     # Define the metric tensor (negative second derivative of the potential term)
     def G(self, x):
-        return self.k + 3*self.lam*x**2
+        '''
+        We define the metric tensor and include a condition that prevents it from being 0
+        '''
+        G = self.k + 3*self.lam*x**2
+        if G < 1e-14:
+          G = 1e-5
+        return G
 
     # Define the kinetic energy term (include correction term)
     def K(self, p, x):
@@ -45,29 +51,32 @@ class RMHMC:
             # Draw the momentum from a Normal distribution
             p = np.random.normal(0, (np.abs(self.G(x[t])))**0.5)
             # Initialise the x_star and p_star lists
-            x_stars = [0]*(self.L+1)
+            x_stars = [x[t]]*(self.L+1)
             p_stars = [p]*(self.L+1)
             # Initialise the p_conv lists
             p_convs = [p,p+1]
             # Use Secant method for fixed point iteration
             # Leapfrog first step
-            for i in range(100):
+            for i in range(1, 100):
+              # We first force the square root to be real
+              inner_val_1 = 1-4*self.eps*(self.G(x_stars[1]))\
+                                        *(p_convs[i-1] + 0.5*self.eps*self.k*x_stars[1] \
+                                          +0.5*self.eps*self.lam*x_stars[1]**3 \
+                                            + 0.25*self.eps*abs(6*self.lam*x_stars[1])/(self.G(x_stars[1])))
+              p_conv_root_1 = np.sqrt(max(0.0, inner_val_1))
+              inner_val_2 = 1-4*self.eps*(self.G(x_stars[2]))\
+                                        *(p_convs[i-2] + 0.5*self.eps*self.k*x_stars[2] \
+                                          +0.5*self.eps*self.lam*x_stars[2]**3 \
+                                            + 0.25*self.eps*abs(6*self.lam*x_stars[2])/(self.G(x_stars[2])))
+              p_conv_root_2 = np.sqrt(max(0.0, inner_val_2))
               p_conv = p_convs[i-1] - 2/(self.eps*(self.G(x_stars[1])))\
-                                *(-1 + (1-4*self.eps*(self.G(x_stars[1]))\
-                                        *(p_convs[i-1] + 0.5*self.eps*self.k*x_stars[1] \
-                                          +0.5*self.eps*self.lam*x_stars[1]**3 \
-                                            + 0.25*self.eps*abs(6*self.lam*x_stars[1])/(self.G(x_stars[1]))))**0.5)\
+                                *(-1+p_conv_root_1)\
                                             *(p_convs[i-1] - p_convs[i-2])/(2/(self.eps*(self.G(x_stars[1])))\
-                                *(-1 + (1-4*self.eps*(self.G(x_stars[1]))\
-                                        *(p_convs[i-1] + 0.5*self.eps*self.k*x_stars[1] \
-                                          +0.5*self.eps*self.lam*x_stars[1]**3 \
-                                            + 0.25*self.eps*abs(6*self.lam*x_stars[1])/(self.G(x_stars[1]))))**0.5)\
+                                *(-1 + p_conv_root_1))\
                                             -(2/(self.eps*(self.G(x_stars[0])))\
-                                *(-1 + (1-4*self.eps*(self.G(x_stars[0]))\
-                                        *(p_convs[i-2] + 0.5*self.eps*self.k*x_stars[0] \
-                                            + 0.25*self.eps*abs(6*self.lam*x_stars[0])/(self.G(x_stars[0]))))**0.5))) 
+                                *(-1 + p_conv_root_2)) 
               p_convs.append(p_conv) 
-              if p_convs[i] - p_stars[i-1] < self.tol:
+              if abs(p_convs[i] - p_convs[i-1]) < self.tol:
                   break 
             p_stars[2] = p_convs[len(p_convs)-1] 
             x_stars[2] = x_stars[1] + self.eps*self.G(x_stars[1])*p_stars[2]
@@ -76,7 +85,8 @@ class RMHMC:
             for j in range(3, self.L+1):
               # Initialise new p_convs list
               p_convs_new = [p_stars[2], p_stars[2]+1]
-              for i in range(100):
+              for i in range(1, 100):
+                # Force the square root to be real 
                 p_conv_new = p_convs_new[i-1] - 1/(self.eps*(self.G(x_stars[j-1])))\
                                 *(-1 + (1-2*self.eps*(self.G(x_stars[j-1]))\
                                         *(p_convs_new[i-1] + self.eps*self.k*x_stars[j-1] \
@@ -93,36 +103,37 @@ class RMHMC:
                                           +0.5*self.eps*self.lam*x_stars[j-2]**3 \
                                             + 0.25*self.eps*abs(6*self.lam*x_stars[j-2])/(self.G(x_stars[j-2]))))**0.5))) 
                 p_convs_new.append(p_conv_new) 
-                if p_convs_new[i] - p_convs_new[i-1] < self.tol:
+                if abs(p_convs_new[i] - p_convs_new[i-1]) < self.tol:
                   break                             
                 p_stars[j] = p_convs_new[len(p_convs_new)-1]
                 x_stars[j] = x_stars[j-1] + self.eps*self.G(x_stars[j-1])*p_stars[j]
             print("p_stars =", p_stars, "x_stars=", x_stars)
             # Leapfrog final step
             # Initialise new p_convs list
-            p_convs_fin = [p_stars[self.L-1], p_stars[self.L]]
-            for i in range(100):
-              p_conv_fin = p_convs_fin[i-1] - 2/(self.eps*(self.G(x_stars[self.L])))\
-                                *(-1 + (1-2*self.eps*(self.G(x_stars[self.L]))\
-                                        *(p_convs_fin[i-1] + 0.5*self.eps*self.k*x_stars[self.L] \
-                                          +0.5*self.eps*self.lam*x_stars[self.L]**3 \
-                                            + 0.25*self.eps*abs(6*self.lam*x_stars[self.L])/(self.G(x_stars[self.L]))))**0.5)\
-                                            *(p_convs_fin[i-1] - p_convs_fin[i-2])/(2/(self.eps*(self.G(x_stars[self.L])))\
-                                *(-1 + (1-2*self.eps*(self.G(x_stars[self.L]))\
-                                        *(p_convs_fin[i-1] + 0.5*self.eps*self.k*x_stars[self.L] \
-                                          +0.5*self.eps*self.lam*x_stars[self.L]**3 \
-                                            + 0.25*self.eps*abs(6*self.lam*x_stars[self.L])/(self.G(x_stars[self.L]))))**0.5)\
-                                            -(2/(self.eps*(self.G(x_stars[self.L-1])))\
+            p_convs_fin = [p_stars[self.L-2], p_stars[self.L-1]]
+            for i in range(1, 100):
+              p_conv_fin = p_convs_fin[i-1] - 2/(self.eps*(self.G(x_stars[self.L-1])))\
                                 *(-1 + (1-2*self.eps*(self.G(x_stars[self.L-1]))\
-                                        *(p_convs_fin[i-2] + 0.5*self.eps*self.k*x_stars[self.L-1] \
+                                        *(p_convs_fin[i-1] + 0.5*self.eps*self.k*x_stars[self.L-1] \
                                           +0.5*self.eps*self.lam*x_stars[self.L-1]**3 \
-                                            + 0.25*self.eps*abs(6*self.lam*x_stars[self.L-1])/(self.G(x_stars[self.L-1]))))**0.5)))
-              if p_convs_fin[i] - p_convs_fin[i-1] < self.tol:
+                                            + 0.25*self.eps*abs(6*self.lam*x_stars[self.L-1])/(self.G(x_stars[self.L-1]))))**0.5)\
+                                            *(p_convs_fin[i-1] - p_convs_fin[i-2])/(2/(self.eps*(self.G(x_stars[self.L-1])))\
+                                *(-1 + (1-2*self.eps*(self.G(x_stars[self.L-1]))\
+                                        *(p_convs_fin[i-1] + 0.5*self.eps*self.k*x_stars[self.L-1] \
+                                          +0.5*self.eps*self.lam*x_stars[self.L-1]**3 \
+                                            + 0.25*self.eps*abs(6*self.lam*x_stars[self.L-1])/(self.G(x_stars[self.L-1]))))**0.5)\
+                                            -(2/(self.eps*(self.G(x_stars[self.L-2])))\
+                                *(-1 + (1-2*self.eps*(self.G(x_stars[self.L-2]))\
+                                        *(p_convs_fin[i-2] + 0.5*self.eps*self.k*x_stars[self.L-2] \
+                                          +0.5*self.eps*self.lam*x_stars[self.L-2]**3 \
+                                            + 0.25*self.eps*abs(6*self.lam*x_stars[self.L-2])/(self.G(x_stars[self.L-2]))))**0.5)))
+              p_convs_fin.append(p_conv_fin)
+              if abs(p_convs_fin[i] - p_convs_fin[i-1]) < self.tol:
                 break
-              p_stars[self.L+1] = p_convs_fin[len(p_convs_fin)-1]
+              p_stars[self.L] = p_convs_fin[len(p_convs_fin)-1]
             print("p_stars =", p_stars, "x_stars=", x_stars)
             # Compute the acceptance ratio
-            r = np.exp(-self.H(x_stars[self.L], p_stars[self.L+1]) + self.H(x[t], p))
+            r = np.exp(-self.H(x_stars[self.L-1], p_stars[self.L]) + self.H(x[t], p))
             # Draw W from a Uniform distribution
             W = np.random.uniform(0, 1)            
             # Carry out the Metropolis test
@@ -133,12 +144,12 @@ class RMHMC:
         return x
 
 # Find the expected value of x
-#def exp_val(x):
+def exp_val(x):
     '''
     Given a list of x values, compute the expected value
     '''
-    #return np.mean(x)
+    return np.mean(x)
 
 # Testing the code
 RMHMC_test = RMHMC(L=10, eps=0.1, k=1, lam=1, tol = 1e-6)
-print(RMHMC_test.RMHMC_alg(10))
+print("Expected value of x =", exp_val(RMHMC_test.RMHMC_alg(10)))
