ab59106aa1a6ff26036ea743334604f219054ecc
Eira is my\ best\ girlfriend <grapes@junk.com>
2026-07-12 17:17:31 +0100

    train

--- weird.py
+++ weird.py
@@ -273,10 +273,10 @@ def RMHMC(L=None,eps=None,k=None,lam=None,tol=None,n=None, d=None):
     acc_rat = (len(accepted)/len(x))*100
     # Plot the anharmonic potential
     burn_in = math.ceil(len(x)/10)
-    # plt.plot(x[burn_in:],PE_vals[burn_in:])
-    # plt.xlabel("x")
-    # plt.ylabel("V(x)")
-    # plt.show()
+    plt.plot(x[burn_in:],PE_vals[burn_in:])
+    plt.xlabel("x")
+    plt.ylabel("V(x)")
+    plt.show()
     return x, KE_vals, PE_vals, exps_delH, errors, acc_rat
     
 # Find the expected value of x and corresponding standardised standard deviation
